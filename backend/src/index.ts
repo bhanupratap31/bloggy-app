b7834c6adedfcccab507d11366f44f5d1bdaa8e9
Bhanu Pratap Shukla <bhanudtg@gmail.com>
2024-12-25 20:42:52 +0530

    Added jwt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL:  string 
+    JWT_SECRET: string
   }
 }>()
 
@@ -18,14 +20,16 @@ app.post('/api/v1/user/signup', async (c) => {
 
   //zod validation needed, password hashing needed
   try{
-    await prisma.user.create({
+    const user = await prisma.user.create({
       data: {
         username: body.username,
         password: body.password, 
         name: body.name,
       }
     })
-    return c.text('User created successfully');
+    const jwt = await sign({ 
+      id: user.id}, c.env.JWT_SECRET);
+    return c.text(jwt);
   }catch(e){
     c.status(411);
     return c.text('Invalid input');
