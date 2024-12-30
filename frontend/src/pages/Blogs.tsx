82559ed3f3681f86dbe2dc042d891ad86ec8ea9f
Bhanu Pratap Shukla <bhanudtg@gmail.com>
2024-12-30 19:55:28 +0530

    Added logic to render the blogs using custom hooks

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,32 +1,26 @@
 import { AppBar } from "../components/AppBar"
 import { BlogCard } from "../components/BlogCard"
+import { useBlogs } from "../hooks"
 
 
 export const Blogs = () => {
+    const [loading, blogs] = useBlogs();
+    if(loading) {
+        return <div>
+            Loading...
+        </div>
+    }
+
     return ( <div>
         <AppBar></AppBar>
         <div className="flex justify-center">
             <div className="max-w-xl">
-                <BlogCard 
-                    authorName={"Bhanu Pratap Shukla"}
-                    title={"How an ugly single page website makes $5000 a month with Affiliate Marketing"} 
-                    content={"How an ugly single page website makes $5000 a month with Affiliate Marketing. How an ugly single page website makes $5000 a month with Affiliate Marketing."}
-                    publishedDate={"2021-10-20"} 
-                />
-
-                <BlogCard 
-                    authorName={"Bhanu Pratap Shukla"}
-                    title={"How an ugly single page website makes $5000 a month with Affiliate Marketing"} 
-                    content={"How an ugly single page website makes $5000 a month with Affiliate Marketing. How an ugly single page website makes $5000 a month with Affiliate Marketing."}
-                    publishedDate={"2021-10-20"} 
-                />
-
-                <BlogCard 
-                    authorName={"Bhanu Pratap Shukla"}
-                    title={"How an ugly single page website makes $5000 a month with Affiliate Marketing"} 
-                    content={"How an ugly single page website makes $5000 a month with Affiliate Marketing. How an ugly single page website makes $5000 a month with Affiliate Marketing."}
-                    publishedDate={"2021-10-20"} 
-                />
+                {blogs.map(blog => <BlogCard 
+                    authorName={blog.author.name || "Anonymous"}
+                    title={blog.title} 
+                    content={blog.content}
+                    publishedDate={blog.publishedDate} 
+                />)}
             </div>
         </div>
     </div>
