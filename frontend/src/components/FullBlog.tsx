5faa49f5f7df4e2150da396f993a3ae209fcedb3
Bhanu Pratap Shukla <bhanudtg@gmail.com>
2024-12-31 20:55:19 +0530

    Working on Full Blog render component

---
+++ frontend/src/components/FullBlog.tsx
@@ -0,0 +1,7 @@
+
+
+export const FullBlog = () => {
+    return <div>
+        FullBlog
+    </div>
+}
--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,9 +1,16 @@
-
+import { FullBlog } from "../components/FullBlog";
+import { useBlog } from "../hooks";
 
 export const Blog = () => {
+    const {loading, blog} = useBlog();
+    if(loading) {
+        return <div>
+            Loading...
+        </div>
+    }
     return (
         <div>
-            blog
+            <FullBlog/>
         </div>
     )
 }
