6b5d1d81407f2e3e8fcd42fa7d7b03c6e451c976
Bhanu Pratap Shukla <bhanudtg@gmail.com>
2025-01-01 17:15:33 +0530

    Fixed the appbar

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -1,14 +1,18 @@
 import { Avatar } from "./BlogCard"
-
+import { Link } from "react-router-dom";
 
 
 export const AppBar = () => {
-    return ( <div className="border-b flex justify-between px-10 py-4">
-        <div className="flex flex-col justify center text-xl font-black"> 
-            Bloggy
-        </div>
-        <div>
-            <Avatar name="Bhanu"></Avatar>
+    return ( <div className="border-b flex justify-between px-10 py-2">
+        <Link to = {'/blogs'} className="mt-2 flex flex-col justify center text-xl font-black cursor-pointer"> 
+           Bloggy
+        </Link>
+        <div className="flex">
+        <button type="button" className="resize mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-extrasmall rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">New</button>
+            <div className="mt-1">
+                <Avatar name="Bhanu"></Avatar>
+            </div>
+            
         </div>
     </div>
     )
--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -39,8 +39,8 @@ export const BlogCard = ({authorName, title, content, publishedDate, id}: BlogCa
 }
 
 export function Avatar ({name}: {name: string }){
-    return <div>
-        <div className="relative inline-flex items-center justify-center w-6 h-6 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+    return <div className="max-w-xl">
+        <div className="relative inline-flex items-center justify-center w-8 h-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
             <span className="font-medium text-gray-600 dark:text-gray-300">{name[0]}</span>
         </div>
     </div>
