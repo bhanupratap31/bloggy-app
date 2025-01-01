4e318a56066b4d84e2992805df2d5bc48f903f49
Bhanu Pratap Shukla <bhanudtg@gmail.com>
2025-01-01 23:15:44 +0530

    Fixed blog.tsx

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -8,7 +8,7 @@ export const Blog = () => {
     const {loading, blog} = useBlog({
         id: id||"1"
     });
-    if(loading) {
+    if(loading || !blog) {
         return <div className="h-screen flex flex-col justify-center">
             <div className="flex justify-center">
                 <BlogSpinner/>
